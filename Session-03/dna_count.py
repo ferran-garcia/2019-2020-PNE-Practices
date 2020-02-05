72c544327d2d5eccccbbd6bb1da7586c28e479a0
ferran <f.garciat.2019@alumnos.urjc.es>
2020-02-05 18:10:49 +0100

    Excercise 2 Session-03

--- Session-03/dna_count.py
+++ Session-03/dna_count.py
@@ -4,7 +4,6 @@ def dna_counter(chain):
     count_T = 0
     count_G = 0
     count_tot = 0
-    count_non = 0
     for e in chain:
         if e in "A":
             count_A += 1
@@ -18,14 +17,12 @@ def dna_counter(chain):
         elif e in "G":
             count_G += 1
             count_tot += 1
-        else:
-            count_non += 1
-            count_tot += 1
+
     print("Total length: ", count_tot)
     print("A: ", count_A)
     print("C: ", count_C)
     print("T: ", count_T)
     print("G: ", count_G)
-    print("The nº of non correct bases is: ", count_non)
 
-dna_counter("CATGTAGACTAGW")
+
+dna_counter("CATGTAGACTAG")
