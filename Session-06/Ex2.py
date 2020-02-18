ed67b9585e5fbad0b9c275645e2f93eb7b2d72fc
ferran <f.garciat.2019@alumnos.urjc.es>
2020-02-18 16:45:42 +0100

    P0

---
+++ Session-06/Ex2.py
@@ -0,0 +1,33 @@
+class Seq:
+    """"A class for representing sequence objects"""
+    def __init__(self, strbases):
+
+        for e in strbases:
+            if e not in ["A", "C", "T", "G"]:
+                print("ERROR!!")
+                self.strbases = "ERROR"
+                return
+        print("New sequence created!")
+        self.strbases = strbases
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+
+
+    pass
+
+class Gene(Seq):
+    pass
+
+def print_seqs(seq_list):
+    for e in range(len(seq_list)):
+        print("Sequence", e, ":")
+        for i in seq_list:
+            print("(Length: ", len(i), ")")
+            print(i)
+
+print_seqs([Seq("ACT"), Seq("GATA"), Seq("CAGATA")])
