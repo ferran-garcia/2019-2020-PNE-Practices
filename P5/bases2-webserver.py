33a847f0bfb54a17b145a2ee91b5a4032128fba8
ferran-garcia <f.garciat.2019@alumnos.urjc.es>
2020-03-24 17:56:26 +0100

    P5

---
+++ P5/bases2-webserver.py
@@ -0,0 +1,80 @@
+import http.server
+import socketserver
+from typing import Any, Union
+
+import termcolor
+from pathlib import Path
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+def read_index(filename):
+    file_lines = Path(filename).read_text()
+    body = file_lines.split("\n")
+    body = body[1:]
+    final_str = ''.join(body)
+    return final_str
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+        folder = "../P5/"
+        try:
+            if self.path == "/" or self.path == "/index.html":
+                contents = read_index(folder + "index.html")
+            else:
+                contents = read_index(folder + self.path[-1] + ".html")
+            self.send_response(200)
+        except FileNotFoundError:
+            contents = read_index(folder + "Error.html")
+            self.send_response(404)
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
