5449a26a2d3e5b9a0ef76f812baf5724bd5e80d1
ferran-garcia <f.garciat.2019@alumnos.urjc.es>
2020-04-21 17:06:05 +0200

    Project

--- Final-Project/final-project.py
+++ Final-Project/final-project.py
@@ -11,6 +11,7 @@ ALWAYS_PARAMS = '?content-type=application/json'
 conn = http.client.HTTPConnection(SERVER_EN)
 # Define the Server's port
 PORT = 8080
+bases = ['A', 'C', 'T', 'G']
 
 
 # -- This is for preventing the error: "Port already in use"
@@ -51,82 +52,129 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 resp1 = conn.getresponse()
                 data_ = resp1.read().decode("utf-8")
                 api_info = json.loads(data_)
-                contents = """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-                    <title>LIST OF SPECIES</title>
-                </head>
-                <body style="background-color: lightblue;">
-                """
-                contents += f"<h2> Species List</h2>"
-                contents += f"<p>The total number of species in ensembl is: {len(api_info['species'])}</p>"
-                if len(args) == 1 or ((args[1])[-1] == '='):
-                    for e in api_info['species']:
-                        contents += f"<p>-{e['display_name']}</p>"
-                else:
-                    second_arg = args[1]
+                if len(args) > 1:
+                    args_2 = args #Lo cambiamos para que no diga index out of range
+                    second_arg = args_2[1]
                     seq_args = second_arg.split("=")
-                    sp_name = api_info['species']
-                    counter = 0
-                    contents += f"<p>The limit the user has selected is: {seq_args[-1]}</p>"
-                    for i in sp_name:
-                        if counter < int(seq_args[-1]):
-                            contents += f"<p>-{i['display_name']}</p>"
-                            counter += 1
-                contents += '<a href="/">Main Page</a>'
-                contents += "</body></html>"
-                self.send_response(200)
+                list_species = []
+                if seq_args[1] == '' or (len(seq_args[1]) == 5 and 'json' in seq_args[1]):
+                    for e in api_info['species']:
+                        list_species.append(e['display_name'])
+                else:
+                    if len(seq_args) == 3:
+                        seq_args = (seq_args[1]).split('&')
+                        sp_name = api_info['species']
+                        counter = 0
+                        for i in sp_name:
+                            if counter < int(seq_args[0]):
+                                list_species.append(i['display_name'])
+                                counter += 1
+                    else:
+                        sp_name = api_info['species']
+                        counter = 0
+                        for i in sp_name:
+                            if counter < int(seq_args[1]):
+                                list_species.append(i['display_name'])
+                                counter += 1
+                if 'json=1' in req_line[1]:
+                    if len(args) == 1 or ('0123456789' not in seq_args[1]):
+                        dict_json = {'ListSpecies': list_species}
+                        contents = json.dumps(dict_json)
+                        self.send_response(200)
+                    else:
+                        dict_json = {'ListSpecies': {'Limit': seq_args[0], 'Species': list_species}}
+                        contents = json.dumps(dict_json)
+                        self.send_response(200)
+                else:
+                    contents = """
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>LIST OF SPECIES</title>
+                    </head>
+                    <body style="background-color: lightblue;">
+                    """
+                    contents += f"<h2> Species List</h2>"
+                    contents += f"<p>The total number of species in ensembl is: {len(api_info['species'])}</p>"
+                    if not len(args) == 1 or ((args[1])[-1] == '='):
+                        contents += f"<p>The limit the user has selected is: {seq_args[-1]}</p>"
+                    for i in list_species:
+                        contents += f"<p>-{i}</p>"
+                    contents += '<a href="/">Main Page</a>'
+                    contents += "</body></html>"
+                    self.send_response(200)
             elif first_arg in '/karyotype':
                 second_arg = args[1]
                 seq_args = second_arg.split("=")
+                if '&' in seq_args[1]:
+                    sp_arg = (seq_args[1]).split('&')
+                    sp_arg = sp_arg[0]
+                else:
+                    sp_arg = seq_args[-1]
                 ENDPOINT = '/info/assembly/'
-                PARAMS = seq_args[-1] + ALWAYS_PARAMS
+                PARAMS = sp_arg + ALWAYS_PARAMS
                 conn.request("GET", ENDPOINT + PARAMS)
                 resp1 = conn.getresponse()
                 data_ = resp1.read().decode("utf-8")
                 api_info = json.loads(data_)
-                contents = """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-                    <title>KARYOTYPE</title>
-                </head>
-                <body style="background-color: lightblue;">
-                """
-                contents += f"<h2> Karyotype of the species: {seq_args[-1]}</h2>"
+                karyotype_list = []
                 for i in api_info['karyotype']:
-                    contents += f"<p>{i}</p>"
-                contents += '<a href="/">Main page</a>'
-                contents += "</body></html>"
+                    karyotype_list.append(i)
+                if 'json=1' in req_line[1]:
+                    dict_json = {'species':{sp_arg:{'chromosomes': karyotype_list}}}
+                    contents = json.dumps(dict_json)
+                else:
+                    contents = """
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>KARYOTYPE</title>
+                    </head>
+                    <body style="background-color: lightblue;">
+                    """
+                    contents += f"<h2> Karyotype of the species: {seq_args[-1]}</h2>"
+                    for e in karyotype_list:
+                        contents += f"<p>{e}</p>"
+                    contents += '<a href="/">Main page</a>'
+                    contents += "</body></html>"
                 self.send_response(200)
             elif first_arg in "/chromosomeLength":
                 second_arg = args[1]
                 seq_args = second_arg.split("=") #Queda la especie junto con el cromosoma
                 args_def = seq_args[1].split("&")   #Separamos especie y cromosoma
+                chromosome = seq_args[-1]
+                if 'json' in seq_args[2]:
+                    args_json = seq_args[2].split('&')
+                    chromosome = args_json[0]
                 ENDPOINT = '/info/assembly/'
                 PARAMS = args_def[0] + ALWAYS_PARAMS
                 conn.request("GET", ENDPOINT + PARAMS)
                 resp1 = conn.getresponse()
                 data_ = resp1.read().decode("utf-8")
                 api_info = json.loads(data_)
-                contents = """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-                    <title>CHROMOSOME LENGTH</title>
-                </head>
-                <body style="background-color: lightblue;">
-                """
-                contents += f"<h2> Chromosome: {seq_args[-1]} Species: {args_def[0]}</h2>"
+                chromo_length = ''
                 for e in api_info['top_level_region']:
-                    if e['name'] == seq_args[-1]:
-                        contents += f"<p>The length of the chromosome is:   {e['length']}</p>"
-                contents += '<a href="/">Main page</a>'
-                contents += "</body></html>"
+                    if e['name'] == chromosome:
+                        chromo_length += str(e['length'])
+                if 'json=1' in req_line[1]:
+                    dict_json = {'species':{args_def[0]:{'Chromosome':{chromosome:{'The length of the chromosome is:': chromo_length}}}}}
+                    contents = json.dumps(dict_json)
+                else:
+                    contents = """
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>CHROMOSOME LENGTH</title>
+                    </head>
+                    <body style="background-color: lightblue;">
+                    """
+                    contents += f"<h2> Chromosome: {seq_args[-1]} Species: {args_def[0]}</h2>"
+                    contents += f"<p>The length of the chromosome is:   {chromo_length}</p>"
+                    contents += '<a href="/">Main page</a>'
+                    contents += "</body></html>"
                 self.send_response(200)
             elif first_arg == "/geneList":
                 ENDPOINT = '/overlap/region/human/'
@@ -151,22 +199,38 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 data_1 = resp1.read().decode("utf-8")
                 api_info_genelist = json.loads(data_1)
                 contents += f"<h2>The genes in the chromosome {values_for_params[0]} that start at {values_for_params[1]} and end at {values_for_params[2]} are:</h2>"
+                gene_in_chromo = []
                 for i in api_info_genelist:
                     contents += f"<p>{i['external_name']}</p>"
+                    gene_in_chromo.append(i['external_name'])
                 contents += '<a href="/">Main page</a>'
                 contents += "</body></html>"
+                if 'json=1' in req_line[1]:
+                    contents = ''
+                    dict_json = {'chromosome': values_for_params[0], 'Start': values_for_params[1], 'End': values_for_params[2], 'Genes': gene_in_chromo}
+                    contents = json.dumps(dict_json)
                 self.send_response(200)
             else:
                 ENDPOINT1 = '/xrefs/symbol/homo_sapiens/'
                 second_arg = args[1]
                 seq_args = second_arg.split("=")
-                PARAMS1 = seq_args[-1] + ALWAYS_PARAMS
+                gene_name = seq_args[1]
+                if 'json' in seq_args[1]:
+                    args_json = seq_args[1].split('&')
+                    gene_name = args_json[0]
+                PARAMS1 = gene_name + ALWAYS_PARAMS
                 conn.request("GET", ENDPOINT1 + PARAMS1)
                 resp1 = conn.getresponse()
                 data_1 = resp1.read().decode("utf-8")
                 api_info_id = json.loads(data_1)
                 gene_id = api_info_id[0]
                 gene_id = gene_id['id']
+                ENDPOINT2 = '/sequence/id/'
+                PARAMS2 = gene_id + ALWAYS_PARAMS
+                conn.request("GET", ENDPOINT2 + PARAMS2)
+                resp2 = conn.getresponse()
+                data_2 = resp2.read().decode("utf-8")
+                api_info_seq = json.loads(data_2)
                 contents = """
                 <!DOCTYPE html>
                 <html lang="en">
@@ -177,39 +241,48 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 <body style="background-color: lightblue;">
                 """
                 if first_arg == "/geneSeq":
-                    ENDPOINT2 = '/sequence/id/'
-                    PARAMS2 = gene_id + ALWAYS_PARAMS
-                    conn.request("GET", ENDPOINT2 + PARAMS2)
-                    resp2 = conn.getresponse()
-                    data_2 = resp2.read().decode("utf-8")
-                    api_info_seq = json.loads(data_2)
-                    contents += f"<h2>The sequence of the gene {seq_args[-1]}</h2>"
-                    contents += f"<textarea>{api_info_seq['seq']}</textarea>"
+                    gene_seq = ''
+                    gene_seq += api_info_seq['seq']  ###Demá seguir tranformant coses desde aqui!!!!!
+                    if 'json=1' in req_line[1]:
+                        dict_json = {'Gene name:':{gene_name:{'Sequence':gene_seq}}}
+                        contents = json.dumps(dict_json)
+                    else:
+                        contents += f"<h2>The sequence of the gene {gene_name}</h2>"
+                        contents += f"<textarea readonly rows = 20 cols = 80>{gene_seq}</textarea>"
+                        contents += '<a href="/">Main page</a>'
+                        contents += "</body></html>"
                 elif first_arg == '/geneInfo':
-                    ENDPOINT2 = '/lookup/id/'
-                    PARAMS2 = gene_id + ALWAYS_PARAMS
-                    conn.request("GET", ENDPOINT2 + PARAMS2)
-                    resp2 = conn.getresponse()
-                    data_2 = resp2.read().decode("utf-8")
-                    api_info_gene = json.loads(data_2)
-                    contents += f"<h2>Information about the gene: {seq_args[-1]}</h2>"
-                    contents += f"<p>The start: {api_info_gene['start']}</p>"
-                    contents += f"<p>The end: {api_info_gene['end']}</p>"
-                    contents += f"<p>This gene is located in the chromosome: {api_info_gene['seq_region_name']}</p>"
-                elif first_arg == "/geneCalc":
-                    ENDPOINT2 = '/sequence/id/'
-                    PARAMS2 = gene_id + ALWAYS_PARAMS
-                    conn.request("GET", ENDPOINT2 + PARAMS2)
-                    resp2 = conn.getresponse()
-                    data_2 = resp2.read().decode("utf-8")
-                    api_info_seq = json.loads(data_2)
+                    ENDPOINT3 = '/lookup/id/'
+                    PARAMS3 = gene_id + ALWAYS_PARAMS
+                    conn.request("GET", ENDPOINT3 + PARAMS3)
+                    resp3 = conn.getresponse()
+                    data_3 = resp3.read().decode("utf-8")
+                    api_info_gene = json.loads(data_3)
                     seq0 = Seq(api_info_seq['seq'])
-                    contents += f"<h2>Some calculations of the gene: {seq_args[-1]}</h2>"
-                    contents += f"<p>The length of the sequence is: {seq0.len()}</p>"
-                    for e in bases:
-                        contents += f"<p>{e} : {seq0.count_base(e)} ({round(seq0.count_base(e)*(100/seq0.len()), 2)}%)</p>"
-                contents += '<a href="/">Main page</a>'
-                contents += "</body></html>"
+                    if 'json=1' in req_line[1]:
+                        dict_json = {'gene': gene_name, 'Start': api_info_gene['start'], 'End': api_info_gene['end'], 'Length': seq0.len(), 'Chromosome': api_info_gene['seq_region_name'], 'ID': gene_id}
+                        contents = json.dumps(dict_json)
+                    else:
+                        contents += f"<h2>Information about the gene: {gene_name}</h2>"
+                        contents += f"<p>The start: {api_info_gene['start']}</p>"
+                        contents += f"<p>The end: {api_info_gene['end']}</p>"
+                        contents += f"<p>The length of the gene's sequence is: {seq0.len()}</p>"
+                        contents += f"<p>This gene is located in the chromosome: {api_info_gene['seq_region_name']}</p>"
+                        contents += f"<p>The ID of the gene is: {gene_id}</p>"
+                        contents += '<a href="/">Main page</a>'
+                        contents += "</body></html>"
+                elif first_arg == "/geneCalc":
+                    seq0 = Seq(api_info_seq['seq'])
+                    if 'json=1' in req_line[1]:
+                        for e in bases:
+                            contents += f"<p>{e} : {seq0.count_base(e)} ({round(seq0.count_base(e) * (100 / seq0.len()), 2)}%)</p>"
+                        dict_json = {'Gene': gene_name, 'Length': seq0.len(), 'A':{seq0.count_base('A'): {'Percentage': (round(seq0.count_base('A') * (100 / seq0.len()), 2))}}, 'C':{seq0.count_base('C'): {'Percentage': (round(seq0.count_base('C') * (100 / seq0.len()), 2))}}, 'T':{seq0.count_base('T'): {'Percentage': (round(seq0.count_base('T') * (100 / seq0.len()), 2))}}, 'G':{seq0.count_base('G'): {'Percentage': (round(seq0.count_base('G') * (100 / seq0.len()), 2))}}}
+                        contents = json.dumps(dict_json)
+                    else:
+                        contents += f"<h2>Some calculations of the gene: {gene_name}</h2>"
+                        contents += f"<p>The length of the sequence is: {seq0.len()}</p>"
+                        for e in bases:
+                            contents += f"<p>{e} : {seq0.count_base(e)} ({round(seq0.count_base(e)*(100/seq0.len()), 2)}%)</p>"
                 self.send_response(200)
 
 
@@ -219,8 +292,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Read the index from th
 
         # Define the content-type header:
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(str.encode(contents)))
+        if 'json=1' in req_line:
+            self.send_header('Content-Type', 'application/json')
+            self.send_header('Content-Length', len(str.encode(contents)))
+
+        else:
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
 
         # The header is finished
         self.end_headers()
